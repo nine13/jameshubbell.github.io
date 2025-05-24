6b77630c62b4671377d7cbc04a50ab538da8201c
nine13 <hubbell13@gmail.com>
2025-05-24 10:11:52 -0400

    updated index.html and main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ anime({
 		{ value: 200, duration: 500 },
 		{ value: 0, duration: 800 },
 	],
+	rotate: {
+		value: "1turn",
+		easing: "easeInOutSine",
+	},
 	delay: function (el, i, l) {
 		return i * 1000;
 	},
